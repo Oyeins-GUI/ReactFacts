2c8ed4eb5428b0ada51b965c6778f7b50f95990c
Oyeins-GUI <oyeinx7@gmail.com>
2022-10-10 08:30:14 +0000

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import logo from '../assets/react.svg'
+
+export default function Navbar() {
+    return (
+        <header className='header'>
+            <nav className='nav'>
+                <div>
+                    <img src={logo} className='logo' />
+                    <h2 className='nav--logo_text'>ReactFacts</h2>
+                </div>
+                <h3 className='nav_title'>React Course - Project 1</h3>
+            </nav>
+        </header>
+    )
+}
